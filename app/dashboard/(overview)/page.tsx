ff520f8f908c82f7a04b4554d921e908334e8854
FreQll <shchepin.man@gmail.com>
2024-03-30 19:41:42 +0200

    skeleton for cards

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -1,4 +1,4 @@
-import { Card } from '@/app/ui/dashboard/cards';
+import CardWrapper, { Card } from '@/app/ui/dashboard/cards';
 import IncomeChart from '@/app/ui/dashboard/income-chart';
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import { poppins } from '@/app/ui/fonts';
@@ -9,21 +9,12 @@ import {
 } from '../../lib/data';
 import { Suspense } from 'react';
 import {
+  CardsSkeleton,
   IncomeChartSkeleton,
   LatestInvoicesSkeleton,
 } from '@/app/ui/skeletons';
 
 export default async function Page() {
-  const income = await fetchIncome();
-  const latestInvoices = await fetchLatestInvoices();
-  const cardData = await fetchCardData();
-  const {
-    totalFulfilledInvoices,
-    totalAwaitingInvoices,
-    numberOfInvoices,
-    numberOfSellers,
-  } = cardData;
-
   return (
     <main className="rounded-xl bg-neutral-900 p-6">
       <h1
@@ -32,14 +23,9 @@ export default async function Page() {
         Dashboard
       </h1>
       <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
-        <Card title="Earned" value={totalFulfilledInvoices} type="earned" />
-        <Card
-          title="In Progress"
-          value={totalAwaitingInvoices}
-          type="awaiting"
-        />
-        <Card title="All Invoices" value={numberOfInvoices} type="invoices" />
-        <Card title="Total Sellers" value={numberOfSellers} type="sellers" />
+        <Suspense fallback={<CardsSkeleton />}>
+          <CardWrapper />
+        </Suspense>
       </div>
       <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
         <Suspense fallback={<IncomeChartSkeleton />}>
--- app/ui/dashboard/cards.tsx
+++ app/ui/dashboard/cards.tsx
@@ -5,6 +5,7 @@ import {
   ClipboardDocumentIcon,
 } from '@heroicons/react/24/outline';
 import { rubik } from '@/app/ui/fonts';
+import { fetchCardData } from '@/app/lib/data';
 
 const iconMap = {
   earned: BanknotesIcon,
@@ -14,14 +15,22 @@ const iconMap = {
 };
 
 export default async function CardWrapper() {
+  const cardData = await fetchCardData();
+  const {
+    numberOfSellers,
+    numberOfInvoices,
+    totalFulfilledInvoices,
+    totalAwaitingInvoices,
+  } = cardData;
+
   return (
     <>
       {/* Attention! Uncomment this section when you reach this stage in the course. */}
 
-      {/* <Card title="Earned" value={totalFulfilledInvoices} type="earned" />
+      <Card title="Earned" value={totalFulfilledInvoices} type="earned" />
       <Card title="In Progress" value={totalAwaitingInvoices} type="awaiting" />
       <Card title="All Invoices" value={numberOfInvoices} type="invoices" />
-      <Card title="Total Sellers" value={numberOfSellers} type="sellers" /> */}
+      <Card title="Total Sellers" value={numberOfSellers} type="sellers" />
     </>
   );
 }
